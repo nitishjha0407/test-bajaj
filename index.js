5942aec46f0a2d6710856afcf85ce00ea987f7f9
nitishjha0407 <nitishjha0407@gmail.com>
2019-03-06 12:56:38 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const express=require('express');
+const app=express();
+app.get('/',(req,res)=>{
+  res.status(200).json('Started');
+})
+app.listen(3000,()=>{
+  console.log('server started');
+})
